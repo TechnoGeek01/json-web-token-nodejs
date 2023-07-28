258e355efd51d4e918fd39e929b5979db2906da4
TechnoGeek01 <rsudharsangames@gmail.com>
2023-07-28 13:40:12 +0530

    jwt token simple project

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const { UnauthenticatedError, BadRequestError } = require("../errors");
+const jwt = require("jsonwebtoken");
+
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new BadRequestError("Invalid Token");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Not Authorized to access this route");
+  }
+};
+
+module.exports = authenticationMiddleware;
